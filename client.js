8a66f69ab2a14d9914e13816f8b4d4aca98291ed
Jason Ihde <jason@quantsig.net>
2013-11-16 15:29:08 +0000

    License formatted.

--- client.js
+++ client.js
@@ -1,11 +1,9 @@
 /*jshint globalstrict: true*/
 /*jshint browser: true*/
 /*!
-This software is distributed under the MIT License.
+Copyright (c) 2013 (Quantitative Signals Network) <support@quantsig.net>
 
 The MIT License (MIT)
-
-Copyright (c) 2013 (Quantitative Signals Network) <support@quantsig.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
